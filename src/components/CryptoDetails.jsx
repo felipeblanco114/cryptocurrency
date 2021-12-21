bdac0b4beb0602a1206b718df90d5e9b4e136f7c
Felipe Blanco Muzzolon <felipeblanco114@gmail.com>
2021-12-21 15:10:21 -0300

    chart

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -1,11 +1,136 @@
-import React from 'react';
+import React, { useState } from 'react';
 import HTMLReactParser from 'html-react-parser';
+import { useParams } from 'react-router-dom';
+import millify from 'millify';
+import { Col, Row, Typography, Select } from 'antd';
+import { MoneyCollectOutlined, DollarCircleOutlined, FundOutlined, ExclamationCircleOutlined, StopOutlined, TrophyOutlined, CheckOutlined, NumberOutlined, ThunderboltOutlined } from '@ant-design/icons'
+import { useGetCryptoDetailsQuery, useGetCryptoHistoryQuery } from '../services/cryptoApi';
+import LineChart from './LineChart';
+
+const { Text, Title } = Typography;
+const { Option } = Select; 
 
 const CryptoDetails = () => {
+
+    const { coinId } = useParams();
+    
+    const [ timePeriod, setTimePeriod ] = useState('7d');
+
+    const { data, isFetching } = useGetCryptoDetailsQuery(coinId);
+    const { data: coinHistory } = useGetCryptoHistoryQuery({ coinId, timePeriod });
+
+    const time = ['3h', '24h', '7d', '30d', '1y', '3m', '3y', '5y'];
+
+    const cryptoDetails = data?.data?.coin;
+
+    if (isFetching) return 'Loading...';
+
+    const stats = [
+      { title: 'Precio a dólares', value: `$ ${cryptoDetails.price && millify(cryptoDetails.price)}`, icon: <DollarCircleOutlined /> },
+      { title: 'Ranking', value: cryptoDetails.rank, icon: <NumberOutlined /> },
+      { title: '24h Volume', value: `$ ${cryptoDetails.volume && millify(cryptoDetails.volume)}`, icon: <ThunderboltOutlined /> },
+      { title: 'Capitalización del mercado', value: `$ ${cryptoDetails.marketCap && millify(cryptoDetails.marketCap)}`, icon: <DollarCircleOutlined /> },
+      { title: 'Máximo histórico(promedio diario)', value: `$ ${millify(cryptoDetails.allTimeHigh.price)}`, icon: <TrophyOutlined /> },
+    ];
+  
+    const genericStats = [
+      { title: 'Número de mercados', value: cryptoDetails.numberOfMarkets, icon: <FundOutlined /> },
+      { title: 'Número de intercambios', value: cryptoDetails.numberOfExchanges, icon: <MoneyCollectOutlined /> },
+      { title: 'Oferta aprovada', value: cryptoDetails.approvedSupply ? <CheckOutlined /> : <StopOutlined />, icon: <ExclamationCircleOutlined /> },
+      { title: 'Oferta total', value: `$ ${millify(cryptoDetails.totalSupply)}`, icon: <ExclamationCircleOutlined /> },
+      { title: 'Oferta circulante', value: `$ ${millify(cryptoDetails.circulatingSupply)}`, icon: <ExclamationCircleOutlined /> },
+    ];
+
     return (
-        <div>
-            
-        </div>
+        <Col className='coin-detail-container'>
+            <Col className='coin-heading-container'>
+                <Title className='coin-name' level={2}>
+                <span style={{ color: `${cryptoDetails.color}` }}>{cryptoDetails.name}</span> ({cryptoDetails.slug}) Precio
+                </Title>
+                <p>
+                    Precio actual del {cryptoDetails.name} en dólares.
+                    Ver estadísticas de valor, capitalización de mercado y oferta
+                </p>
+            </Col>
+            <Select 
+                defaultValue='7d' 
+                className='select-timeperiod' 
+                placeholder='Selecciona período de tiempo' 
+                onChange={(value) => setTimePeriod(value)}
+            >
+                {time.map((date) => (
+                    <Option key={date}>
+                        {date}
+                    </Option>
+                ))}
+            </Select>
+            <LineChart coinHistory={coinHistory} currentPrice={millify(cryptoDetails.price)} coinName={cryptoDetails.name} /> 
+            <Col className='stats-container'>
+                <Col className='coin-value-statistics'>
+                    <Col className='coin-value-statistics-heading'>
+                        <Title level={3} className='coin-details-heading'>
+                            Estadísticas de valor del {cryptoDetails.name}.
+                        </Title>
+                        <p>
+                            Una descripción general de las estadísticas del {cryptoDetails.name}.
+                        </p>
+                    </Col>
+                    {stats.map(({icon, title, value}) => (
+                        <Col className='coin-stats'>
+                            <Col className='coin-stats-name'>
+                                <Text>{icon}</Text>
+                                <Text>{title}</Text>
+                            </Col>
+                            <Text className='stats'>{value}</Text>
+                        </Col>
+                    ))}
+                </Col>
+            </Col>
+            <Col className='other-stats-info'>
+                <Col className='coin-value-statistics'>
+                    <Col className='coin-value-statistics-heading'>
+                        <Title level={3} className='coin-details-heading'>
+                            Otras estadísticas.
+                        </Title>
+                        <p>
+                            Una descripción general de las estadísticas de las criptomonedas.
+                        </p>
+                    </Col>
+                    {genericStats.map(({icon, title, value}) => (
+                        <Col className='coin-stats'>
+                            <Col className='coin-stats-name'>
+                                <Text>{icon}</Text>
+                                <Text>{title}</Text>
+                            </Col>
+                            <Text className='stats'>{value}</Text>
+                        </Col>
+                    ))}
+                </Col>
+            </Col>
+            <Col className='coin-desc-link'>
+                <Row className='coin-desc'>
+                    <Title level={3} className='coin-details-heading'>
+                        ¿Qué es {cryptoDetails.name}?
+                        {HTMLReactParser(cryptoDetails.description)}
+                    </Title>
+                </Row>
+                <Col className='coin-links'>
+                    <Title level={3} className='coin-details-heading'>
+                        {cryptoDetails.name} Links.
+                    </Title>
+                    {cryptoDetails.links.map((link) => (
+                        <Row className='coin-link' key={link.name}>
+                            <Title level={5} className='link-name'>
+                                {link.type}
+                            </Title>
+                            <a href={link.url} target='_blank' rel='noreferrer'>
+                                {link.name}
+                            </a>
+                        </Row>
+                    ))}
+                </Col>
+            </Col>
+        </Col>      
     )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Button, Menu, Typography, Avatar } from 'antd';
 import { Link } from 'react-router-dom';
 import { HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOutlined } from '@ant-design/icons';
@@ -6,6 +6,28 @@ import { HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOut
 import icon from '../images/mc.png';
 
 const Navbar = () => {
+
+    const [activeMenu, setActiveMenu] = useState(true);
+    const [screenSize, setScreenSize] = useState(null);
+
+    useEffect(() => {
+        const handleResize = () => setScreenSize(window.innerWidth);
+
+        window.addEventListener('resize', handleResize);
+
+        handleResize();
+
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
+    useEffect(() => {
+        if(screenSize < 760){
+            setActiveMenu(false);
+        } else {
+            setActiveMenu(true);
+        }
+    }, [screenSize])
+
     return (
         <div className='nav-container'>
             <div className='logo-container'>
@@ -14,23 +36,25 @@ const Navbar = () => {
                     <Link to='/'>MiCrypto</Link>
                 </Typography.Title>
             </div>
-            {/* <Button className='menu-control-container'>
-
-            </Button> */}
-            <Menu style={{ backgroundColor: '#ffdf2c', borderRight: 'none' }}>
-                <Menu.Item icon={<HomeOutlined />}>
-                    <Link to='/'>Home</Link>
-                </Menu.Item>
-                <Menu.Item icon={<FundOutlined />}>
-                    <Link to='/cryptocurrencies'>Criptomonedas</Link>
-                </Menu.Item>
-                <Menu.Item icon={<MoneyCollectOutlined />}>
-                    <Link to='/exchanges'>Intercambios</Link>
-                </Menu.Item>
-                <Menu.Item icon={<BulbOutlined />}>
-                    <Link to='/news'>Noticias</Link>
-                </Menu.Item>
-            </Menu>
+            <Button className='menu-control-container' onClick={() => setActiveMenu(!activeMenu)}>
+                <MenuOutlined />
+            </Button>
+            { activeMenu && 
+                (<Menu style={{ backgroundColor: '#ffdf2c', borderRight: 'none' }}>
+                    <Menu.Item icon={<HomeOutlined />}>
+                        <Link to='/'>Home</Link>
+                    </Menu.Item>
+                    <Menu.Item icon={<FundOutlined />}>
+                        <Link to='/cryptocurrencies'>Criptomonedas</Link>
+                    </Menu.Item>
+                    <Menu.Item icon={<MoneyCollectOutlined />}>
+                        <Link to='/exchanges'>Intercambios</Link>
+                    </Menu.Item>
+                    <Menu.Item icon={<BulbOutlined />}>
+                        <Link to='/news'>Noticias</Link>
+                    </Menu.Item>
+                </Menu> )
+            }
         </div>
     )
 }
